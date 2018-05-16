94aefead03ae884061e0f7d7bd198b628d57d03f
Cem <cemhekimoglu.ch@gmail.com>
2018-05-16 20:31:22 +0000

    fixed the bug

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import ChatBar from './ChatBar.jsx';
 import MessageList from './MessageList.jsx';
 import NavBar from './NavBar.jsx';
 
+const uuidv4 = require('uuid/v4');
+
 
 class App extends Component {
 
@@ -11,33 +13,38 @@ class App extends Component {
     // this is the *only* time you should assign directly to state:
     this.state = {
       currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
-      messages: [
-        {
-          id: 1,
-          username: "Bob",
-          content: "Has anyone seen my marbles?",
-        },
-        {
-          id: 2,
-          username: "Anonymous",
-          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
-        }
-      ]
+      messages: []
     };
   }
 
   componentDidMount() {
     console.log("componentDidMount <App />");
-    setTimeout(() => {
-      console.log("Simulating incoming message");
+    // setTimeout(() => {
+    //   console.log("Simulating incoming message");
       // Add a new message to the list of messages in the data store
-      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-      const messages = this.state.messages.concat(newMessage)
-      // Update the state of the app component.
-      // Calling setState will trigger a call to render() in App and all child components.
-      this.setState({messages: messages})
-    }, 3000);
+      // const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+      // const messages = this.state.messages.concat(newMessage);
+      this.socket = new WebSocket(
+        process.env.REACT_APP_SOCKET_SERVER || "ws://localhost:3001",
+      );
+
+      // Connection opened
+      this.socket.onopen = event => {
+        this.socket.send("{\"message\":\"Hello Server\"}");
+      }
+
+      // Listen for messages
+      const EVENT_MESSAGE_KEY = "message";
+    // }, 3000);
   }
+
+  addMessage=(message) => {
+    const messages = this.state.messages.concat(message)
+    console.log(messages);
+    this.setState({messages: messages})
+    this.socket.send(JSON.stringify(message))
+  }
+
 
 
   render() {
@@ -45,7 +52,7 @@ class App extends Component {
       <div>
         <NavBar/>
         <MessageList messages={this.state.messages}/>
-        <ChatBar currentUser={this.state.currentUser}/>
+        <ChatBar currentUser={this.state.currentUser} addMessage={this.addMessage}/>
       </div>
     );
   }
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -3,12 +3,18 @@ import React, {Component} from 'react';
 class ChatBar extends Component {
 
 
+  handleKeyPress = (event) => {
+    if(event.key == 'Enter'){
+      this.props.addMessage({username: this.props.currentUser.name, content: event.target.value})
+      console.log('enter press here! ')
+    }
+  }
 
   render() {
     return (
       <footer className="chatbar">
         <input className="chatbar-username" defaultValue={this.props.currentUser.name} placeholder="Your Name (Optional)" />
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+        <input className="chatbar-message" onKeyPress={this.handleKeyPress} placeholder="Type a message and hit ENTER" />
       </footer>
     );
   }
